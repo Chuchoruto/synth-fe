4b1ad1d56c0c248dc164f9ff2411d873d8f4ca97
nxdavar <nealrr01@gmail.com>
2024-08-25 18:35:34 -0400

    fix(all pages): remove whitespace from bottom by dynamically resizing based on viewport size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
+'use client';
+
 // external imports:
 import { Container } from '@mui/material';
 import '@fontsource-variable/outfit';
 import '@fontsource-variable/overpass';
+import { useMediaQuery } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
 // internal imports:
 import CustomAppBar from './components/CustomAppBar';
 import CustomStepper from './components/CustomStepper';
 
 export default function Home() {
+  const theme = useTheme();
+
+  const isXsOrMd = useMediaQuery(theme.breakpoints.down('md'));
+
+  const contHeight = {
+    height: isXsOrMd ? '100vh' : '100%'
+  };
+
   return (
-    <Container maxWidth={false} sx={styles.topLevelCont}>
+    <Container maxWidth={false} sx={{ ...styles.topLevelCont, ...contHeight }}>
       <CustomAppBar />
       <CustomStepper />
     </Container>
@@ -19,6 +31,7 @@ export default function Home() {
 const styles = {
   topLevelCont: {
     padding: '0px !important',
+    margin: '0px !important',
     backgroundColor: '#C9D5C3',
     height: '100vh'
   },
